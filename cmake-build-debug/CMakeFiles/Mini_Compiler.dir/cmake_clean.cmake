e0e4219cf5cd1b5d364150573f6435edf3cdc58a
jeevanthask <jeevanthanandika@gmail.com>
2021-04-16 08:24:16 +0530

    initial project created

---
+++ cmake-build-debug/CMakeFiles/Mini_Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mini_Compiler.dir/main.c.obj"
+  "Mini_Compiler.exe"
+  "Mini_Compiler.exe.manifest"
+  "Mini_Compiler.pdb"
+  "libMini_Compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mini_Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
